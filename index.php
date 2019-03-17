2b7eb6c08d8eb7422ad1427660068ce403fd34a0
Sylvain Julé <sylvain.jule@free.fr>
2019-03-17 17:44:33 +0100

    Follow the new Pluginkit guidelines

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+Kirby::plugin('sylvainjule/locator', array(
+	'options' => array(
+		'token'        => '',
+		'id'           => 'mapbox.outdoors',
+		'tiles'        => 'wikimedia',
+		'geocoding'    => 'nominatim',
+		'display'      => array('lat','lon','number','address','postcode','city','country'),
+		'draggable'    => true,
+		'autocomplete' => true,
+		'zoom.min'     => 2,
+		'zoom.default' => 12,
+		'zoom.max'     => 18,
+		'center.lat'   => 48.864716,
+		'center.lon'   => 2.349014,
+		'liststyle'    => 'table',
+		'marker'       => 'dark',
+	),
+	'fields'       => require_once __DIR__ . '/lib/fields.php',
+	'fieldMethods' => require_once __DIR__ . '/lib/fieldMethods.php',
+    'translations' => array(
+        'de' => require_once __DIR__ . '/lib/languages/de.php',
+        'en' => require_once __DIR__ . '/lib/languages/en.php',
+        'fr' => require_once __DIR__ . '/lib/languages/fr.php',
+    ),
+));

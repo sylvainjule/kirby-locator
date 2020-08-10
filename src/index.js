5eb4a5d50e97809e9a05e27ab9dc33c30e51b749
Markus Gerards <mail@markus-gerards.de>
2020-08-10 10:37:48 +0200

    Field preview for structure fields

--- src/index.js
+++ src/index.js
@@ -5,4 +5,35 @@ panel.plugin('sylvainjule/locator', {
     fields: {
         locator: Locator,
     },
+    components: {
+        'k-locator-field-preview': {
+            props: {
+                value: Object,
+                column: Object,
+                field: Object
+            },
+
+            computed: {
+                fieldPreview() {
+                    let vm = this;
+                    let fields = vm.column.fields;
+                    let output = [];
+
+                    if (fields === undefined) {
+                        fields = ['city', 'country'];
+                    }
+
+                    fields.forEach(function(field) {
+                        if (vm.value[field] !== undefined) {
+                            output.push(vm.value[field]);
+                        }
+                    });
+
+                    return output.join(', ');
+                }
+            },
+
+            template: '<p class="k-structure-table-text">{{fieldPreview}}</p>'
+        }
+    }
 });

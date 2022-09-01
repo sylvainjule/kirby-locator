f60366c0fa2252a978dd1b610291b5ed031621ff
Ahmet Bora <byybora@gmail.com>
2022-09-01 19:32:09 +0300

    Add TR language

---
+++ lib/languages/tr.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	'locator.placeholder'    => 'Konum ara',
+	'locator.locate'         => 'Bul',
+	'locator.collapse'       => 'Küçült',
+	'locator.latitude'       => 'Enlem',
+	'locator.longitude'      => 'Boylam',
+	'locator.number'         => 'Sokak Numarası',
+	'locator.address'        => 'Adres',
+	'locator.postcode'       => 'Posta Kodu',
+	'locator.city'           => 'Şehir',
+  'locator.region'         => 'Bölge',
+	'locator.country'        => 'Ülke',
+  'locator.osm'            => 'OSM ID',
+	'locator.empty'          => 'Görüntülenecek bir konum yok.',
+	'locator.empty_response' => 'Konumu bulamadık. Lütfen daha fazla ayrıntı ekleyin.',
+	'locator.error'          => 'Bir şeyler yanlış gitti. Tekrar deneyin veya yöneticinizle iletişime geçin.',
+  'locator.reset'          => 'Sıfırla',
+);
